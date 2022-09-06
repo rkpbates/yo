3076d3cf9e3adcbd105fbc48ae184d889fc66622
rkpbates <112780672+rkpbates@users.noreply.github.com>
2022-09-06 15:19:30 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.IO;
 
 class ScanTube
@@ -7,48 +10,61 @@ class ScanTube
     {
         try
         {
-            using (StreamReader sr = new StreamReader(@"C:\Users\batesr4\Desktop\Index\Tube Transfer Files\scan1.csv"))
+            using (StreamReader sr1 = new StreamReader(@"C:\Users\batesr4\Desktop\Index\Tube Transfer Files\scan1.csv"))
             using (StreamReader sr2 = new StreamReader(@"C:\Users\batesr4\Desktop\Index\Tube Transfer Files\scan2.csv"))
             {
                 string line;
                 string line2;
 
-                while ((line = sr.ReadLine()) != null && (line2 = sr2.ReadLine()) != null)
+                while ((line = sr1.ReadLine()) != null && (line2 = sr2.ReadLine()) != null)
                 {
                     string[] lineValues1 = line.Split(new char[] { ';' });
                     string[] lineValues2 = line2.Split(new char[] { ';' });
-                    // columns for first .csv
-                    string column01 = lineValues1[0];
-                    string column02 = lineValues1[1];
-                    string column03 = lineValues1[2];
-                    string column04 = lineValues1[3];
-                    string column05 = lineValues1[4];
-                    string column06 = lineValues1[5];
-                    string column07 = lineValues1[6];
-                    // columns for second .csv
-                    string column08 = lineValues2[0];
-                    string column09 = lineValues2[1];
-                    string column10 = lineValues2[2];
-                    string column11 = lineValues2[3];
-                    string column12 = lineValues2[4];
-                    string column13 = lineValues2[5];
-                    string column14 = lineValues2[6];
+                    //Columns for first .csv
+                    string gridPos = lineValues1[0];
+                    string siteOnGrid = lineValues1[1];
+                    string tipNumber = lineValues1[2];
+                    string configName = lineValues1[3];
+                    string rackLabel = lineValues1[4];
+                    string carrierBarcode = lineValues1[5];
+                    string srcRackID = lineValues1[6];
+                    //Columns for second .csv
+                    string gridPos2 = lineValues2[0];
+                    string siteOnGrid2 = lineValues2[1];
+                    string tipNumber2 = lineValues2[2];
+                    string configName2 = lineValues2[3];
+                    string rackLabel2 = lineValues2[4];
+                    string carrierBarcode2 = lineValues2[5];
+                    string srcRackID2 = lineValues2[6];
 
-                    if (lineValues1[5] != lineValues2[5])
-                        //Specifically comparing Barcode columns between the .csv files
-                    {
-                        Console.WriteLine("Does not match {0}, {1}", column06, column13);
-                    }
+                    int total = 0;
+                    if (lineValues1[5] == lineValues2[5] && lineValues1[5] != ("CarrierBarcode") && lineValues2[5] != ("CarrierBarcode"))
+                    //Specifically comparing Barcode columns between the .csv files
+                        {
+                            Console.WriteLine("Match: {0}, {1}", carrierBarcode, carrierBarcode2);
+                            total += 0;
+                        //continue;
+                        }
+                    else if(lineValues1[5] != lineValues2[5])
+                        {
+                            Console.WriteLine("Does not match: {0}, {1}", carrierBarcode, carrierBarcode2);
+                            total += 1;
+                        }
                     else
-                    {
-                        Console.WriteLine("Match");
-                    }
+                        {
+
+                        }
+                    using (StreamWriter sw = new StreamWriter(@"C:\Users\batesr4\Desktop\Index\Tube Transfer Files\temp.txt"))
+                        {
+                        sw.WriteLine(total);
+                        }
+
                 }
             }
         }
         catch
-        {
-
+        {   
         }
     }  
 }
+
